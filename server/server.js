e9a17ed2fe2b116cbe97e5150c97fdbbc98af3b0
fangyh1994 <31147954+fangyh1994@users.noreply.github.com>
2018-11-23 19:16:26 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,9 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail);
-    // });
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createAt: new Date().getTime()
+    });
+
+    // socket.broadcast.emit from admin to new user joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
@@ -26,6 +35,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // })
     });
     socket.on('disconnect', () => {
         console.log('User was disconnected');
