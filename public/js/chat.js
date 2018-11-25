e4de217617f9484561e156f9e39bf59eece53440
fangyh1994 <31147954+fangyh1994@users.noreply.github.com>
2018-11-24 21:28:59 -0500

    Send message to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -56,7 +56,7 @@ socket.on('updateUserList', function (users) {
 
 
 socket.on('newMessage', function (message) {
-    var formattedTime = moment(message.createdAt).format('h:mm a');
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var template = jQuery('#message-template').html();
   var html = Mustache.render(template, {
     text: message.text,
